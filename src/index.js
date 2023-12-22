fd104f368e472561a2e9f19e10926f2531006121
Ray <rayoneill.97@gmail.com>
2023-12-22 10:18:57 -0500

    Added additional styling for mobile views; added code to translate nav menu on button click in mobile view; replaced footer;

--- src/index.js
+++ src/index.js
@@ -18,4 +18,39 @@ function scrollHideHeader() {
   lastYPosition = scrollDelta;
 }
 
-document.addEventListener('scroll', () => scrollHideHeader());
+
+
+const linkItems = document.querySelectorAll('.header-link-item');
+linkItems.forEach(element => {
+  const borderElement = element.querySelector('.bottom-border');
+
+  element.addEventListener('mouseover', () => {
+    borderElement.classList.add('is-hovered');
+  });
+
+  element.addEventListener('mouseout', () => {
+    borderElement.classList.remove('is-hovered');
+  });
+});
+
+const mobileMenuDropdown = document.querySelector('.dropdown-button');
+const mobileMenu = document.querySelector('.nav-section');
+
+mobileMenuDropdown.addEventListener('click', () => {
+
+  // if the menu is already dropped
+  if(mobileMenu.classList.contains('is-dropped')) {
+    // turn = into X
+  } else {
+    // turn X into =
+  }
+
+  mobileMenu.classList.toggle('is-displayed');
+})
+
+document.addEventListener('scroll', () => {
+  if (mobileMenu.classList.contains('is-displayed')) {
+    mobileMenu.classList.remove('is-displayed');
+  }
+  scrollHideHeader()
+});
